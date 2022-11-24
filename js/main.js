1d16a3319c9e50d74a43aa9ba57ffcaf7c679c9c
Judit Closa <jclosac@inscastellet.cat>
2022-11-24 19:46:43 +0100

    Main page template working

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
 //Init
+let projects = [];
 $(document).ready(function () {
+    //Load projects
+    projects.push(new ProjectCard("Test project","Test","Interesting decription decription decription decription decription decription decription decription decription decription decription decription decription decription decription decription decription decription decription decription decription decription decription decription decription decription decription decription decription in her e e e e e e e e e e e e e e e e e e e e e","Youtube/drive","https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png","github url"));
+
+    projects.forEach(element => {
+        $(".projectDiv").append(element.returnDom());
+    });
+    //Set main page
     setMainPage();
+
+    //Menu
     $(".nav-link").filter(function () {
         if ($(this).text() == "Projects") {
             $(this).addClass("active");
         }
 
     });
+    
 
 });
 
@@ -17,11 +28,16 @@ $(".navbar .nav-link").on("click", function () {
     if ($(this).text() == "Projects") {
         setMainPage();
     }
+    
 
 });
 
 
 function setMainPage() {
+    $(".projectDiv").show();
+    $(".curriculumDiv").hide();
+    $(".contactDiv").hide();
+    
     console.log("Main page loaded");
     
 }
--- js/projectCard.js
+++ js/projectCard.js
@@ -1,5 +1,22 @@
 class ProjectCard{
     constructor(title,subtitle,description,videoUrl,screenShotUrl,gitHubUrl){
-
+        this.title = title;
+        this.subtitle = subtitle;
+        this.description = description;
+        this.videoUrl = videoUrl;
+        this.screenShotUrl = screenShotUrl;
+        this.gitHubUrl = gitHubUrl;
+    }
+    returnDom() {
+        
+        return `<div class="card">
+                    <div class="imgdiv"><img src="${this.screenShotUrl}"></img></div>
+                    <div class="textdiv"><h1>${this.title}</h1>
+                        <h2>${this.subtitle}</h2>
+                        <p>${this.description}</p>
+                        <div class="iconlinks"><i class="fa-brands fa-youtube"><a class="icotext" href="${this.videoUrl}">Video</a></i>
+                        <i class="fa-brands fa-github"><a class="icotext" href="${this.gitHubUrl}">GitHub</a></i></div>
+                    </div>
+                </div>`;
     }
 }

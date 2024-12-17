5e724d0b9945994c9193a84043ceacc741890e47
judit <juditclosa@gmail.com>
2024-12-17 16:49:26 +0100

    New Project

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     //Load projects
     projects.push(new ProjectCard("Just Scannit","Project for M7","Semi functional website build with react,features some pages, with simulated register and login in localstorage","https://youtu.be/zsj_LKY01YA","sources\\jsi.png","https://github.com/JClosaInscastellet/JustScanIt"));
     projects.push(new ProjectCard("RemADHDers","Telegram Bot","Speech to text bot that acts as a pseudo-calendar","https://drive.google.com/file/d/1B59UVKbxcFPxTbunlSsefPWbggq87KRf/view?usp=sharing","sources\\tllogo.png","https://github.com/ProfessionalJudit/Telegram_Bot"));
-
+projects.push(new ProjectCard("Tower Meowfense","Unity Game","Deckbuilding tower defense game in wich you can also atack the enemy!","https://youtu.be/zi35v0mBR7k","https://cdn.freebiesupply.com/logos/large/2x/unity-69-logo-black-and-white.png","https://github.com/ProfessionalJudit/MeowFense"));
     //Add to dom
     projects.forEach(element => {
         $(".projectDiv").append(element.returnDom());
